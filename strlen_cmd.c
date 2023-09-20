b7b1da31b1bf977fe174118785ec2f5aeb3915e1
Augustine <austynomilano77@gmail.com>
2023-09-20 10:58:08 -0700

    string  length

---
+++ strlen_cmd.c
@@ -0,0 +1,17 @@
+#include "Dshell.h"
+
+/**
+  *strlen_cmd - Entry to the function
+  *@string: pointer to a string
+  *Return: the initial counter
+  */
+
+int strlen_cmd(char *string)
+{
+	int start = 0;
+
+	while (string[start] != '\0')
+		start++;
+
+		return (start);
+}
